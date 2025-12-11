23cf7bff272d8112ab5b6ce9c3ce86235d72bf37
Pravin Jadhav <pravin@gmail.com>
2025-12-11 12:22:15 +0530

    Fixed controller path

--- backend/routes/parking.js
+++ backend/routes/parking.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const sendEmail = require("./backend/utils/sendEmail");
+const sendEmail = require("../utils/sendEmail");
 
 const { vehicleEntry, vehicleExit, completePayment } = require("../controllers/parkingcontroller");
 
